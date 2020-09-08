cbc47d1d99ad1fcbaf5beeaffc3ed2f5422c9d73
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-08 19:02:20 -0300

    Added argument parser

--- src/django_rest_starter/__main__.py
+++ src/django_rest_starter/__main__.py
@@ -1,9 +1,18 @@
 import sys
 
+from argparse import ArgumentParser
 from typing import Any
 
 def main() -> Any:
-    pass
+    parser = ArgumentParser()
+    parser.add_argument('project_name', help='project name')
+    parser.add_argument('--user', help='add user template', default=False, const=True, nargs="?")
+    parser.add_argument('--heroku', help='add heroku config', default=False, const=True, nargs="?")
+    args = parser.parse_args()
+    if(args.user):
+        pass
+    if(args.heroku):
+        pass
 
 if __name__ == "__main__":
     sys.exit(main())
