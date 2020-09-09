5b310a023f25fc40a6fdd7b1d3a88133051d500d
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-09 13:26:30 -0300

    Email and description changend in setup file and added get requirements method with objective of retrieve the especified requirements of requirements.json file to install

--- src/django_rest_starter/_venv.py
+++ src/django_rest_starter/_venv.py
@@ -1,4 +1,15 @@
+import os
+import json
 
 class Venv:
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path + '\\.venv\\Scripts\\'
+
+    def _get_requirements(self, key):
+        path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(path)
+        with open('requirements.json', 'r') as json_file:
+            req_file = json.load(json_file)
+            req = req_file[key]
+            req = " ".join(req)
+        return req
