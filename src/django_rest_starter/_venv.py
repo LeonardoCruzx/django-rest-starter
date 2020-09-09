d36bf7a5f5d6ec4dfad09237d84c6fc31fea3e31
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-09 13:38:02 -0300

    Added type hiting to methods of Venv class and added method install requirements which has the aim of install requirements

--- src/django_rest_starter/_venv.py
+++ src/django_rest_starter/_venv.py
@@ -2,14 +2,19 @@ import os
 import json
 
 class Venv:
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path + '\\.venv\\Scripts\\'
 
-    def _get_requirements(self, key):
+    def _get_requirements(self, requirements: str) -> str:
         path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(path)
         with open('requirements.json', 'r') as json_file:
             req_file = json.load(json_file)
-            req = req_file[key]
+            req = req_file[requirements]
             req = " ".join(req)
         return req
+
+    def install_requirements(self, requirements: str) -> None:
+        req = self._get_requirements(requirements)
+        os.chdir(self.path)
+        os.system(f"pip install {req}")
