6421f866fdbdce95e62b824dbea3e0b76900abc3
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-10 17:56:45 -0300

    Added comments to the main file

--- src/django_rest_starter/__main__.py
+++ src/django_rest_starter/__main__.py
@@ -14,14 +14,23 @@ def main() -> Any:
     parser.add_argument('--user', help='add user template', default=False, const=True, nargs="?")
     parser.add_argument('--heroku', help='add heroku config', default=False, const=True, nargs="?")
     args = parser.parse_args()
+    
+    #creates dir of the project
     django = Django(args.project_name)
     django.create_project_dir()
-    venv = Venv()
+
+    #creates .venv and install the base requirements
+    venv = Venv(django.project_path)
     venv.create_venv()
     venv.install_requirements("base_requirements")
+
+    #use django admin to start the project
     django.create_project()
-    settings = Settings(django.project_path + f"\\{args.project_name}")
+
+    #add base settings to the project
+    settings = Settings(django.project_path + f"\\{django.project_name}")
     settings.add_settings("base_settings")
+
     if(args.user):
         user = User(django.project_path)
         user.add_template("base_template")
