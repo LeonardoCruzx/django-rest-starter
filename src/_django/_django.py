4230c20b7c59b4c34fc39b27f72e41f8f51f3f67
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-10 18:48:09 -0300

    Django add template method refactored

--- src/_django/_django.py
+++ src/_django/_django.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 class Django:
     def __init__(self, project_name):
         self.project_name = project_name
@@ -9,3 +10,14 @@ class Django:
     def create_project_dir(self):
         os.makedirs(self.project_name)
         self.project_path = os.getcwd() + f"\\{self.project_name}"
+
+    def add_template(self, template: str) -> None:
+        path = os.path.dirname(os.path.abspath(__file__))
+        path += f"\\{template}"
+        self._copy_tree(path, self.project_path)
+
+    def _copy_tree(self, src, dst):
+        for f in os.listdir(src):
+            s = os.path.join(src, f)
+            d = os.path.join(dst, f)
+            shutil.copy2(s,d)
