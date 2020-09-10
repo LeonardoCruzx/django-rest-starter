a1c4c0c60f964e64721ad44d92fa5f327ec0f69b
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-10 10:53:30 -0300

    Added get settings method to settings class

--- src/django_rest_starter/_settings.py
+++ src/django_rest_starter/_settings.py
@@ -1,3 +1,12 @@
 class Settings:
     def __init__(self):
         pass
+
+    def _get_settings(self, settings: str) -> str:
+        path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(path)
+        with open('settings.json', 'r') as json_file:
+            sett_file = json.load(json_file)
+            sett = sett_file[settings]
+            sett = " ".join(sett)
+        return sett
