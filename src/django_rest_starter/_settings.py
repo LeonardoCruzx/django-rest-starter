4eda485167661fd20ab5995dbca4da4bb5a5236b
leonardo <leonardo.m.cruz@hotmail.com>
2020-09-10 11:51:14 -0300

    Fixed settings class, added requirements and settings

--- src/django_rest_starter/_settings.py
+++ src/django_rest_starter/_settings.py
@@ -1,6 +1,18 @@
+import os
+import json
+
 class Settings:
-    def __init__(self):
-        pass
+    def __init__(self, settings_path):
+        self.path = settings_path
+
+    def add_settings(self, settings: str):
+        settings = self._get_settings(settings)
+        os.chdir(self.path)
+        with open('settings.py', 'a') as pf:
+            for s in settings:
+                pf.write("\n")
+                for k,v in s.items():
+                    pf.write(v + "\n")
 
     def _get_settings(self, settings: str) -> str:
         path = os.path.dirname(os.path.abspath(__file__))
@@ -8,5 +20,4 @@ class Settings:
         with open('settings.json', 'r') as json_file:
             sett_file = json.load(json_file)
             sett = sett_file[settings]
-            sett = " ".join(sett)
         return sett
